850689c39b4f98963db59f6b383ec21777beac3b
Jarl Gullberg <jarlg.gullberg@gmail.com>
2016-01-04 17:32:51 +0100

    Renamed CalculateSectorKey to GetFileKey.

--- WarLib/MPQ/Crypto/MPQCrypt.cs
+++ WarLib/MPQ/Crypto/MPQCrypt.cs
@@ -214,7 +214,7 @@ namespace WarLib.MPQ.Crypto
 		/// <param name="isAdjusted">If set to <c>true</c>, the key is adjusted by the given block offset and file size.</param>
 		/// <param name="blockOffset">The block offset of the file.</param>
 		/// <param name="fileSize">The size of the file.</param>
-		public static uint CalculateSectorKey(string fileName, bool isAdjusted = false, uint blockOffset = 0, uint fileSize = 0)
+		public static uint GetFileKey(string fileName, bool isAdjusted = false, uint blockOffset = 0, uint fileSize = 0)
 		{
 			uint fileKey = Hash(fileName, HashType.FileKey);
 
