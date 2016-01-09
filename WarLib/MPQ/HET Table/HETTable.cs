8aac57d214d856388f83a971349a43c6e52ab4ab
Jarl Gullberg <jarlg.gullberg@gmail.com>
2016-01-09 14:39:21 +0100

    Added stub classes for BET and HET tables.

---
+++ WarLib/MPQ/HET Table/HETTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WarLib.MPQ.HETTable
+{
+	public class HETTable
+	{
+		private uint Signature;
+		private uint Version;
+		private uint DataSize;
+
+		private uint TableSize;
+		private uint HashTableSize;
+		private uint HashEntrySize;
+		private uint TotalIndexSize;
+		private uint IndexSizeExtra;
+		private uint IndexSize;
+		private uint BlockTableSize;
+
+		private List<byte> HashTable;
+		private List<ulong> FileIndices;
+
+		public HETTable(byte[] data)
+		{
+		}
+	}
+}
+
