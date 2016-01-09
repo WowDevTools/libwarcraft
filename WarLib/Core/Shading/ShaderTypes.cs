c8ceecd89e42afaaf4ff9545ebd2fcb79c781f15
Jarl Gullberg <jarlg.gullberg@gmail.com>
2016-01-09 15:09:17 +0100

    Added the shader types available. n. 15 is still unclear.

--- WarLib/Core/Shading/ShaderTypes.cs
+++ WarLib/Core/Shading/ShaderTypes.cs
@@ -4,6 +4,23 @@ namespace WarLib.Core.Shading
 {
 	public enum ShaderTypes
 	{
+		Diffuse = 0,
+		Specular = 1,
+		Metal = 2,
+		Env = 3,
+		Opaque = 4,
+		EnvMetal = 5,
+		TwoLayerDiffuse = 6,
+		TwolayerEnvMetal = 7,
+		TwoLayerTerrain = 8,
+		DiffuseEmissive = 9,
+		WaterWindow = 10,
+		MaskedEnvMetal = 11,
+		EnvMetalEmissive = 12,
+		TwoLayerDiffuseOpaque = 13,
+		TwoLayerDiffuseEmissive = 14,
+		DiffuseTerrain = 16,
+		AdditiveMaskedEnvMetal = 17
 	}
 }
 
