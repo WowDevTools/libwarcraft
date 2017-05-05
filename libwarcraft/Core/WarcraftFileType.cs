e35888e5b85ec8e0ecd85d23ab3ebf049f3d8a3d
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-05-05 12:58:47 +0200

    Made file types a flag type instead.

--- libwarcraft/Core/WarcraftFileType.cs
+++ libwarcraft/Core/WarcraftFileType.cs
@@ -20,165 +20,168 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
+using System;
+
 namespace Warcraft.Core
 {
 	/// <summary>
 	/// A statically typed set of filetypes that can be encountered in the Warcraft archives. This allows the program
 	/// to easily distinguish file types and associated functionality.
 	/// </summary>
-	public enum WarcraftFileType
+	[Flags]
+	public enum WarcraftFileType : uint
 	{
 		/// <summary>
-		/// Unknown files. This file type hasn't been entered into the source code yet.
+		/// Unknown file. This file type hasn't been entered into the source code yet.
 		/// </summary>
-		Unknown,
+		Unknown									= 0x0,
 
 		/// <summary>
 		/// A directory. This is not a file.
 		/// </summary>
-		Directory,
+		Directory								= 0x1,
 
 		/// <summary>
 		/// A manifest containing a list of files which belong to an addon.
 		/// .toc
 		/// </summary>
-		AddonManifest,
+		AddonManifest							= 0x2,
 
 		/// <summary>
 		/// A manifest signature to verify the integrity of a manifest.
 		/// .toc.sig
 		/// </summary>
-		AddonManifestSignature,
+		AddonManifestSignature					= 0x4,
 
 		/// <summary>
 		/// A "Mike O'Brien Package", also known as an MPQ archive.
 		/// .mpq
 		/// </summary>
-		MoPaQArchive,
+		MoPaQArchive							= 0x8,
 
 		/// <summary>
 		/// A configuration file.
 		/// .wtf
 		/// </summary>
-		ConfigurationFile,
+		ConfigurationFile						= 0x10,
 
 		/// <summary>
 		/// A client database container.
 		/// .dbc
 		/// </summary>
-		DatabaseContainer,
+		DatabaseContainer						= 0x20,
 
 		/// <summary>
 		/// A shader container.
 		/// .bls
 		/// </summary>
-		Shader,
+		Shader									= 0x40,
 
 		/// <summary>
 		/// Water definitions for the terrain files.
 		/// .wlw
 		/// </summary>
-		TerrainWater,
+		TerrainWater							= 0x80,
 
 		/// <summary>
 		/// Liquid definitions for the terrain files.
 		/// .wlq
 		/// </summary>
-		TerrainLiquid,
+		TerrainLiquid							= 0x100,
 
 		/// <summary>
 		/// Level or map file, containing the layout and general information about a map.
 		/// .wdl
 		/// </summary>
-		TerrainLevel,
+		TerrainLevel							= 0x200,
 
 		/// <summary>
 		/// Table of all terrain tiles in a map.
 		/// .wdt
 		/// </summary>
-		TerrainTable,
+		TerrainTable							= 0x400,
 
 		/// <summary>
 		/// Terrain tile, containing actual terrain data.
 		/// .adt
 		/// </summary>
-		TerrainData,
+		TerrainData								= 0x800,
 
 		/// <summary>
 		/// Binary image in BLP format.
 		/// .blp
 		/// </summary>
-		BinaryImage,
+		BinaryImage								= 0x1000,
 
 		/// <summary>
 		/// Hash translation table. Used to translate from minimap paths to MD5 hashes and back.
 		/// .trs
 		/// </summary>
-		Hashmap,
+		Hashmap									= 0x2000,
 
 		/// <summary>
 		/// Game object that isn't huge and can be animated. Commonly known as M2 files.
 		/// .m2, .mdx
 		/// </summary>
-		GameObjectModel,
+		GameObjectModel							= 0x4000,
 
 		/// <summary>
 		/// World object root file. Huge, static, no animations. Commonly known as WMO files.
 		/// .wmo
 		/// </summary>
-		WorldObjectModel,
+		WorldObjectModel						= 0x8000,
 
 		/// <summary>
 		/// Group files for world object files. This is where the actual model data lives.
 		/// name_000-512.wmo
 		/// </summary>
-		WorldObjectModelGroup,
+		WorldObjectModelGroup					= 0x10000,
 
 		// Here come a few "normal" files
 		/// <summary>
 		/// WAV-compressed audio.
 		/// </summary>
-		WaveAudio,
+		WaveAudio								= 0x20000,
 
 		/// <summary>
 		/// MP3-compressed audio.
 		/// </summary>
-		MP3Audio,
+		MP3Audio								= 0x40000,
 
 		/// <summary>
 		/// EXtensible Markup Language file.
 		/// </summary>
-		XML,
+		XML										= 0x80000,
 
 		/// <summary>
 		/// JPEG image file.
 		/// </summary>
-		JPGImage,
+		JPGImage								= 0x100000,
 
 		/// <summary>
 		/// Animated GIF file. It's pronounced with a hard G.
 		/// </summary>
-		GIFImage,
+		GIFImage								= 0x200000,
 
 		/// <summary>
 		/// PNG image.
 		/// </summary>
-		PNGImage,
+		PNGImage								= 0x400000,
 
 		/// <summary>
 		/// INI configuration file.
 		/// </summary>
-		INI,
+		INI										= 0x800000,
 
 		/// <summary>
 		/// PDF presentation file.
 		/// </summary>
-		PDF,
+		PDF										= 0x1000000,
 
 		/// <summary>
 		/// Hypertext file. Commonly known as a website.
 		/// </summary>
-		HTML
+		HTML									= 0x2000000
 	}
 }
 
