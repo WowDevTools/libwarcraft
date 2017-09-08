d85b4cd448d7fc616399a74f64029e370f319045
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-09-08 22:34:55 +0200

    Use inheritance for record loading integration tests.

---
+++ libwarcraft.Tests/Integration/DBC/RecordLoadingTests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Warcraft.Core;
+using Warcraft.DBC.Definitions;
+
+namespace libwarcraft.Tests.Integration.DBC
+{
+	[TestFixture]
+	public abstract class RecordLoadingTests
+	{
+		protected WarcraftVersion Version;
+
+		[SetUp]
+		public abstract void Setup();
+
+		private static void TestLoadRecord<T>(WarcraftVersion version) where T : DBCRecord, new()
+		{
+			var databaseName = DBCTestHelper.GetDatabaseNameFromRecordType(typeof(T));
+			if (!DBCTestHelper.HasDatabaseFile(version, databaseName))
+			{
+				Assert.Ignore("Database file not present. Skipping.");
+			}
+
+			var database = DBCTestHelper.LoadDatabase<T>(version, databaseName);
+
+			try
+			{
+				var _ = database.First();
+			}
+			catch (ArgumentException e)
+			{
+				Assert.Fail($"Failed to read {databaseName}: {e}");
+			}
+		}
+
+		[Test]
+		public void AnimationDataRecord()
+		{
+			TestLoadRecord<AnimationDataRecord>(this.Version);
+		}
+
+		[Test]
+		public void CharHairGeosetsRecord()
+		{
+			TestLoadRecord<CharHairGeosetsRecord>(this.Version);
+		}
+
+		[Test]
+		public void SoundAmbienceRecord()
+		{
+			TestLoadRecord<SoundAmbienceRecord>(this.Version);
+		}
+
+		[Test]
+		public void CharSectionsRecord()
+		{
+			TestLoadRecord<CharSectionsRecord>(this.Version);
+		}
+
+		[Test]
+		public void SoundEntriesRecord()
+		{
+			TestLoadRecord<SoundEntriesRecord>(this.Version);
+		}
+
+		[Test]
+		public void CreatureDisplayInfoExtraRecord()
+		{
+			TestLoadRecord<CreatureDisplayInfoExtraRecord>(this.Version);
+		}
+
+		[Test]
+		public void SoundProviderPreferences()
+		{
+			TestLoadRecord<SoundProviderPreferencesRecord>(this.Version);
+		}
+
+		[Test]
+		public void CreatureDisplayInfoRecord()
+		{
+			TestLoadRecord<CreatureDisplayInfoRecord>(this.Version);
+		}
+
+		[Test]
+		public void SpellRecord()
+		{
+			TestLoadRecord<SpellRecord>(this.Version);
+		}
+
+		[Test]
+		public void CreatureModelDataRecord()
+		{
+			TestLoadRecord<CreatureModelDataRecord>(this.Version);
+		}
+
+		[Test]
+		public void WMOAreaTableRecord()
+		{
+			TestLoadRecord<WMOAreaTableRecord>(this.Version);
+		}
+
+		[Test]
+		public void LiquidObjectRecord()
+		{
+			TestLoadRecord<LiquidObjectRecord>(this.Version);
+		}
+
+		[Test]
+		public void ZoneIntroMusicTableRecord()
+		{
+			TestLoadRecord<ZoneIntroMusicTableRecord>(this.Version);
+		}
+
+		[Test]
+		public void LiquidTypeRecord()
+		{
+			TestLoadRecord<LiquidTypeRecord>(this.Version);
+		}
+
+		[Test]
+		public void ZoneMusicRecord()
+		{
+			TestLoadRecord<ZoneMusicRecord>(this.Version);
+		}
+
+		[Test]
+		public void MapRecord()
+		{
+			TestLoadRecord<MapRecord>(this.Version);
+		}
+	}
+}
--- libwarcraft.Tests/Integration/DBC/Wrath/RecordLoadingTests.cs
+++ libwarcraft.Tests/Integration/DBC/Wrath/RecordLoadingTests.cs
@@ -1,133 +1,15 @@
-﻿using System;
-using System.Linq;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Warcraft.Core;
-using Warcraft.DBC.Definitions;
 
 namespace libwarcraft.Tests.Integration.DBC.Wrath
 {
 	[TestFixture]
-	public class RecordLoadingTests
+	public class WrathLoadingTests : RecordLoadingTests
 	{
-		private const WarcraftVersion Version = WarcraftVersion.Wrath;
-
-		private static void TestLoadRecord<T>(WarcraftVersion version) where T : DBCRecord, new()
+		[SetUp]
+		public override void Setup()
 		{
-			var databaseName = DBCTestHelper.GetDatabaseNameFromRecordType(typeof(T));
-			if (!DBCTestHelper.HasDatabaseFile(version, databaseName))
-			{
-				Assert.Ignore("Database file not present. Skipping.");
-			}
-
-			var database = DBCTestHelper.LoadDatabase<T>(version, databaseName);
-
-			try
-			{
-				var _ = database.First();
-			}
-			catch (ArgumentException e)
-			{
-				Assert.Fail($"Failed to read {databaseName}: {e}");
-			}
-		}
-
-		public class LoadRecord
-		{
-			[Test]
-			public void AnimationDataRecord()
-			{
-				TestLoadRecord<AnimationDataRecord>(Version);
-			}
-
-			[Test]
-			public void CharHairGeosetsRecord()
-			{
-				TestLoadRecord<CharHairGeosetsRecord>(Version);
-			}
-
-			[Test]
-			public void SoundAmbienceRecord()
-			{
-				TestLoadRecord<SoundAmbienceRecord>(Version);
-			}
-
-			[Test]
-			public void CharSectionsRecord()
-			{
-				TestLoadRecord<CharSectionsRecord>(Version);
-			}
-
-			[Test]
-			public void SoundEntriesRecord()
-			{
-				TestLoadRecord<SoundEntriesRecord>(Version);
-			}
-
-			[Test]
-			public void CreatureDisplayInfoExtraRecord()
-			{
-				TestLoadRecord<CreatureDisplayInfoExtraRecord>(Version);
-			}
-
-			[Test]
-			public void SoundProviderPreferences()
-			{
-				TestLoadRecord<SoundProviderPreferencesRecord>(Version);
-			}
-
-			[Test]
-			public void CreatureDisplayInfoRecord()
-			{
-				TestLoadRecord<CreatureDisplayInfoRecord>(Version);
-			}
-
-			[Test]
-			public void SpellRecord()
-			{
-				TestLoadRecord<SpellRecord>(Version);
-			}
-
-			[Test]
-			public void CreatureModelDataRecord()
-			{
-				TestLoadRecord<CreatureModelDataRecord>(Version);
-			}
-
-			[Test]
-			public void WMOAreaTableRecord()
-			{
-				TestLoadRecord<WMOAreaTableRecord>(Version);
-			}
-
-			[Test]
-			public void LiquidObjectRecord()
-			{
-				TestLoadRecord<LiquidObjectRecord>(Version);
-			}
-
-			[Test]
-			public void ZoneIntroMusicTableRecord()
-			{
-				TestLoadRecord<ZoneIntroMusicTableRecord>(Version);
-			}
-
-			[Test]
-			public void LiquidTypeRecord()
-			{
-				TestLoadRecord<LiquidTypeRecord>(Version);
-			}
-
-			[Test]
-			public void ZoneMusicRecord()
-			{
-				TestLoadRecord<ZoneMusicRecord>(Version);
-			}
-
-			[Test]
-			public void MapRecord()
-			{
-				TestLoadRecord<MapRecord>(Version);
-			}
+			this.Version = WarcraftVersion.Wrath;
 		}
 	}
 }
