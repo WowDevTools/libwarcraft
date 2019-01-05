04042a238a141a99bc229e08f6c850d8ff06efd3
Jarl Gullberg <Jarl.Gullberg@gmail.com>
2019-01-05 17:46:28 +0100

    More general cleanup.

--- libwarcraft/DBC/Definitions/SpellAttributeF.cs
+++ libwarcraft/DBC/Definitions/SpellAttributeF.cs
@@ -1,3 +1,22 @@
+//
+//  SpellAttributeF.cs
+//
+//  Copyright (c) 2018 Jarl Gullberg
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 using System;
 
 namespace Warcraft.DBC.Definitions
@@ -58,7 +77,7 @@ namespace Warcraft.DBC.Definitions
         /// TODO: Unknown behaviour
         /// This spell starts a periodic tick when it is applied.
         /// </summary>
-        StartPeriodicTickWhenApplied= 0x00000200,
+        StartPeriodicTickWhenApplied = 0x00000200,
 
         /// <summary>
         /// The duration of this spell is hidden for the client.
